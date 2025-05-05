5a167e467460a8e9f61d0250becef806601f3eff
Axelalan05 <axeldavidalan05@gmail.com>
2025-05-05 21:20:23 +0000

    tp4-axel-alan

---
+++ tp4-axel/src/components/Contenedor.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <select v-model="componenteSeleccionado">
+            <option value="Lista">Lista</option>
+            <option value="Tareas">Tareas</option>
+            <option value="DimensionComponente">Dimension</option>
+        </select>
+
+        <keep-alive>
+            <component :is="componentes[componenteSeleccionado]" />
+        </keep-alive>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Lista from './Lista.vue'
+import Tareas from './Tareas.vue'
+import DimensionComponente from './DimensionComponente.vue'
+
+const componenteSeleccionado = ref<'Lista' | 'Tareas' | 'DimensionComponente'>('Lista')
+
+const componentes = {
+    Lista,
+    Tareas,
+    DimensionComponente
+}
+
+</script>
+
+<style scoped></style>
---
+++ tp4-axel/src/components/DimensionComponente.vue
@@ -0,0 +1,31 @@
+import { onMounted } from 'vue';
+<template>
+    <div>
+        <h2>Dimensiones del componente</h2>
+        <p>Dimension: {{ width }} x {{ height }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const width = ref(window.innerWidth)
+const height = ref(window.innerHeight)
+
+function actualizarDimensiones() {
+    width.value = window.innerWidth
+    height.value = window.innerHeight
+}
+
+onMounted(() => {
+    window.addEventListener('resize', actualizarDimensiones)
+})
+
+onUnmounted(() => {
+    window.removeEventListener('resize', actualizarDimensiones)
+})
+
+</script>
+
+<style scoped></style>
